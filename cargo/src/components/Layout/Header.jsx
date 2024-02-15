96a00fd3611eacd2b3a21b65a69132f7eb1ae938
woraput <woraput@kkumail.com>
2024-02-16 00:25:46 +0700

    update padding margin layout components

--- cargo/src/components/Layout/Header.jsx
+++ cargo/src/components/Layout/Header.jsx
@@ -7,30 +7,32 @@ import TextLink from "../Typography/TextLink";
 
 export default function Header() {
   return (
-    <div className="mx-auto flex h-[72px] w-full max-w-[1440px] items-center justify-between bg-neutral-white px-10 py-3 drop-shadow-md">
-      <div className="flex h-12 items-center gap-6">
-        <img src={Logo} alt="logo" />
-        <H4 className={"px-4 py-[10.5px] text-blue"}>Home</H4>
-        <H4Light
-          className={"px-4 py-[10.5px] font-normal text-neutral-secondary"}
-        >
-          สินค้าพร้อมส่ง
-        </H4Light>
-        <div className="flex w-[120px] flex-col gap-1 rounded-lg bg-cyan-50 px-2 py-[6.5px]">
-          <Caption className={"text-right text-blue-300"}>
-            เรทเงินวันนี้
-          </Caption>
-          <Caption className={"text-right text-sm text-blue"}>
-            {" "}
-            1 CNY = 5 THB{" "}
-          </Caption>
+    <div className="bg-neutral-white drop-shadow-md">
+      <div className="mx-auto flex h-[72px] w-full max-w-[1440px] items-center justify-between px-10 py-3">
+        <div className="flex h-12 items-center gap-6">
+          <img src={Logo} alt="logo" />
+          <H4 className={"px-4 py-[10.5px] text-blue"}>Home</H4>
+          <H4Light
+            className={"px-4 py-[10.5px] font-normal text-neutral-secondary"}
+          >
+            สินค้าพร้อมส่ง
+          </H4Light>
+          <div className="flex w-[120px] flex-col gap-1 rounded-lg bg-cyan-50 px-2 py-[6.5px]">
+            <Caption className={"text-right text-blue-300"}>
+              เรทเงินวันนี้
+            </Caption>
+            <Caption className={"text-right text-sm text-blue"}>
+              {" "}
+              1 CNY = 5 THB{" "}
+            </Caption>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center gap-2 px-4">
-        <i class="las la-user text-[32px] text-blue"></i>
-        <TextLink className={"text-blue"}>เข้าสู่ระบบ</TextLink>
-        <span className="text-neutral-secondary">|</span>
-        <TextLink className="text-blue">สมัครสมาชิก</TextLink>
+        <div className="flex items-center gap-2 px-4">
+          <i class="las la-user text-[32px] text-blue"></i>
+          <TextLink className={"text-blue"}>เข้าสู่ระบบ</TextLink>
+          <span className="text-neutral-secondary">|</span>
+          <TextLink className="text-blue">สมัครสมาชิก</TextLink>
+        </div>
       </div>
     </div>
   );
