558ce4edde0aa880c65874311a2175bea30adce9
guntza10 <jomphop.saibuatong@hotmail.com>
2024-02-15 18:42:24 +0700

    implement ButtonOutline

---
+++ cargo/src/components/Common/ButtonOutline.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styled from "@emotion/styled";
+
+const ButtonStyled = styled.button`
+  min-width: 240px;
+`;
+
+function ButtonOutline({ children, className, ...props }) {
+  return (
+    <ButtonStyled
+      className={`border border-solid border-primary-blue-1 p-3 rounded-lg text-primary-blue-1 ${className}`}
+      {...props}
+    >
+      {children}
+    </ButtonStyled>
+  );
+}
+
+export default ButtonOutline;
