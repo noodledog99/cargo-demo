0d884f9ffe12cec334e887e18a2c2f267f075de2
guntza10 <jomphop.saibuatong@hotmail.com>
2024-02-16 00:30:08 +0700

    implement SelectedProduct

--- cargo/src/pages/ProductDetail/components/SelectedProduct.jsx
+++ cargo/src/pages/ProductDetail/components/SelectedProduct.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-import { Card } from "../../../components/Common/Card/Card";
+import Card from "../../../components/Common/Card/Card";
+import BodyText from "../../../components/Typography/BodyText";
 
-function SelectedProductCard({ data }) {
-  return <Card></Card>;
+function SelectedProductCard({ data, ...props }) {
+  return (
+    <Card className="inline-block" {...props}>
+      <img className="inline-block" src={data.img} />
+      <BodyText className="mx-2">{data.name}</BodyText>
+    </Card>
+  );
 }
 
 export default SelectedProductCard;
