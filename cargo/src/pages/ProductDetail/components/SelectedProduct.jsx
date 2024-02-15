ddcbb056b52e37b3a8a4fe8fb118c733b6ac9023
guntza10 <jomphop.saibuatong@hotmail.com>
2024-02-15 23:53:53 +0700

    initiate SelectedProductCard

---
+++ cargo/src/pages/ProductDetail/components/SelectedProduct.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Card } from "../../../components/Common/Card/Card";
+
+function SelectedProductCard({ data }) {
+  return <Card></Card>;
+}
+
+export default SelectedProductCard;
