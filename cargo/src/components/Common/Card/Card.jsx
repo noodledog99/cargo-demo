6fcc925b75071e1e4e6fb6efd98757c0c25a48df
guntza10 <jomphop.saibuatong@hotmail.com>
2024-02-16 01:38:38 +0700

    refactor card

--- cargo/src/components/Common/Card/Card.jsx
+++ cargo/src/components/Common/Card/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card({ children, onClick, className, ...props }) {
   return (
     <div
-      className={`overflow-hidden rounded-lg border border-neutral-line ${className} ${onClick && "cursor-pointer"}`}
+      className={`overflow-hidden rounded-lg border border-neutral-line  ${onClick && "cursor-pointer"} ${className}`}
       onClick={onClick}
       {...props}
     >
