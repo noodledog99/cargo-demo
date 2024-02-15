c64486a8d6de485cde31d7134509922020a35029
kittanya.sri <kittanya.sri@gmail.com>
2024-02-16 01:23:50 +0700

    implement card design

--- cargo/src/components/Common/Card/CardProduct.jsx
+++ cargo/src/components/Common/Card/CardProduct.jsx
@@ -5,6 +5,7 @@ import H4Light from "../../Typography/H4Light";
 import styled from "@emotion/styled";
 
 import ThialandIcon from "../../../assets/icons/thailand.svg";
+import Caption from "../../Typography/Caption";
 
 const Div = styled.div`
   .tag-round {
@@ -15,20 +16,20 @@ const Div = styled.div`
 export default function CardProduct({ productItem, ...props }) {
   return (
     <Div {...props}>
-      <Card>
+      <Card className={"hover:border-2 hover:border-cyan"}>
         <div className="product-content">
           {/*  */}
           <div className="img-content relative w-full">
             <div className="tag tag-round absolute top-1 z-10 flex bg-cyan-blue pb-2 pl-1 pr-2 pt-2 opacity-80">
-              <BodyText className={"mr-1 font-bold text-neutral-white"}>
+              <Caption className={"mr-1 font-bold text-neutral-white"}>
                 พร้อมส่งจากไทย
-              </BodyText>
+              </Caption>
               <img src={ThialandIcon} alt="thailand_icon" className="w-4" />
             </div>
             <img src={productItem.product_image} className="product-img " />
           </div>
           {/*  */}
-          <div className="mx-1 mt-2 gap-2 pb-4 pl-2 pr-2 pt-0">
+          <div className="gap-2 px-1 pb-4 pl-2 pr-2 pt-2 hover:bg-cyan-50 ">
             <H4Light className="product-title">
               {productItem.product_name}
             </H4Light>
