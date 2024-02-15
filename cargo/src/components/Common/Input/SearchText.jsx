d53eb351f0c315e7465da1c154e3820c6fca7e5f
guntza10 <jomphop.saibuatong@hotmail.com>
2024-02-15 21:47:39 +0700

    implement SearchText

--- cargo/src/components/Common/Input/SearchText.jsx
+++ cargo/src/components/Common/Input/SearchText.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 import styled from "@emotion/styled";
 
+const Div = styled.div`
+  .position-top {
+    top: 1rem;
+  }
+`;
 const InputStyled = styled.input``;
 
 function SearchText({ className, ...props }) {
   return (
-    <div className="relative w-full">
-      <i class="las la-search la-lg absolute left-2 top-5 text-neutral-secondary"></i>
-      <i class="lar la-image la-lg absolute right-4 top-5 text-neutral-secondary"></i>
+    <Div className="relative w-full">
+      <i className="las la-search la-lg position-top absolute left-2 text-neutral-secondary"></i>
+      <i className="lar la-image la-lg position-top absolute right-4 text-neutral-secondary"></i>
       <InputStyled
         className={`w-full rounded-lg border border-neutral-hint px-8 py-3 text-base ${className}`}
         {...props}
       />
-    </div>
+    </Div>
   );
 }
 
