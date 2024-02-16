7eb720a67a4eb5b3a86bd1a3af9a921b1ef3ffd8
kittanya.sri <kittanya.sri@gmail.com>
2024-02-16 17:55:40 +0700

    add discount to product card

--- cargo/src/components/Common/Card/CardProduct.jsx
+++ cargo/src/components/Common/Card/CardProduct.jsx
@@ -11,6 +11,10 @@ const Div = styled.div`
   .tag-round {
     border-radius: 0px 20px 20px 0px;
   }
+
+  .discount {
+    font-size: 0.625rem;
+  }
 `;
 
 export default function CardProduct({ productItem, ...props }) {
@@ -26,13 +30,23 @@ export default function CardProduct({ productItem, ...props }) {
               </Caption>
               <img src={ThialandIcon} alt="thailand_icon" className="w-4" />
             </div>
-            <img src={productItem.product_image} className="product-img w-full object-cover h-44" />
+            <img
+              src={productItem.product_image}
+              className="product-img h-44 w-full object-cover"
+            />
           </div>
           {/*  */}
           <div className="gap-2 px-1 pb-4 pl-2 pr-2 pt-2 hover:bg-cyan-50 ">
             <H4Light className="product-title">
               {productItem.product_name}
             </H4Light>
+            {productItem.reduced_price !== 0 && (
+              <Caption
+                className={"mr-2 font-normal text-neutral-hint line-through"}
+              >
+                {`฿ ${productItem.reduced_price}`}
+              </Caption>
+            )}
             <BodyText
               className={"text-blue"}
             >{`฿ ${productItem.price}`}</BodyText>
@@ -45,9 +59,19 @@ export default function CardProduct({ productItem, ...props }) {
                   <i className="las la-star-half-alt h-4 w-4 text-cyan"></i>
                   <i className="lar la-star h-4 w-4 text-cyan"></i>
                 </div>
-                <BodyText className="rate-text text-sm text-neutral-secondary">
+
+                <BodyText className="rate-text mr-2 text-sm text-neutral-secondary">
                   {productItem.rate}
                 </BodyText>
+                {productItem.discount && (
+                  <div className="flex justify-center rounded bg-accent-red ">
+                    <BodyText
+                      className={
+                        "discount px-1 py-1 font-bold text-neutral-white"
+                      }
+                    >{`ลด ${productItem.discount}`}</BodyText>
+                  </div>
+                )}
               </div>
             </div>
           </div>
